0d9009f8812646d87aceb92fcda93e2ac8df00dd
Vaisakh K M <68694876+Vaisakhkm2625@users.noreply.github.com>
2022-01-18 19:38:47 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+  { name: "Perspiciatis", image: "/pwatry/images/coffee1.jpg" },
+  { name: "Voluptatem", image: "/pwatry/images/coffee2.jpg" },
+  { name: "Explicabo", image: "/pwatry/images/coffee3.jpg" },
+  { name: "Rchitecto", image: "/pwatry/images/coffee4.jpg" },
+  { name: " Beatae", image: "/pwatry/images/coffee5.jpg" },
+  { name: " Vitae", image: "/pwatry/images/coffee6.jpg" },
+  { name: "Inventore", image: "/pwatry/images/coffee7.jpg" },
+  { name: "Veritatis", image: "/pwatry/images/coffee8.jpg" },
+  { name: "Accusantium", image: "/pwatry/images/coffee9.jpg" },
 ]
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
